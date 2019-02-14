8e63da8a65c33efb0be38c7d7267b38b749c01e6
Alex Sandiiarov <sandiyarov@gmail.com>
2019-02-14 14:08:14 -0600

    fix: 🐛 Fix cli

--- src/index.ts
+++ src/index.ts
@@ -6,46 +6,51 @@ import fs from 'fs';
 // @ts-ignore
 import { addMockFunctionsToSchema, removeMockFunctionsFromSchema } from 'lunar-core';
 
-program
-  .version('0.1.1', '-v, --version')
-  .option('-p, --port <n>')
-  .option('-s, --schema <n>')
-  .parse(process.argv);
+export default async function() {
+  program
+    .version('0.0.2', '-v, --version')
+    .usage('[options] <file ...>')
+    .option('-p, --port <port>')
+    .parse(process.argv);
 
-const { port, schema: file } = program;
+  const {
+    port,
+    args: [file],
+  } = program;
 
-const toFunction = (str: string) => Function.call(null, `return ${str}`)();
+  const toFunction = (str: string) => Function.call(null, `return ${str}`)();
 
-const deserialize = (body: { [name: string]: any }) =>
-  Object.keys(body).reduce(
-    (packet, key) => ({ ...packet, [key]: toFunction(body[key]) }),
-    {}
-  );
+  const deserialize = (body: { [name: string]: any }) =>
+    Object.keys(body).reduce(
+      (packet, key) => ({ ...packet, [key]: toFunction(body[key]) }),
+      {}
+    );
 
-const schema = makeExecutableSchema({
-  typeDefs: fs.readFileSync(file, 'utf8'),
-});
+  const schema = makeExecutableSchema({
+    typeDefs: fs.readFileSync(file, 'utf8'),
+  });
 
-const currentMocks = [{}];
-const app = express();
-const server = new ApolloServer({ schema });
+  const currentMocks = [{}];
+  const app = express();
+  const server = new ApolloServer({ schema });
 
-app.use(bodyParser.json());
-app.get('/', (req, res) => res.json({ port, schema }));
-app.post('/mock', (req, res) => {
-  currentMocks.push(deserialize(req.body));
-  addMockFunctionsToSchema({ schema, mocks: currentMocks });
-  res.sendStatus(200);
-});
-app.post('/reset', (req, res) => {
-  currentMocks.splice(1, currentMocks.length);
-  removeMockFunctionsFromSchema({ schema });
-  addMockFunctionsToSchema({ schema, mocks: {} });
-  res.sendStatus(200);
-});
+  app.use(bodyParser.json());
+  app.get('/', (req, res) => res.json({ port, schema }));
+  app.post('/mock', (req, res) => {
+    currentMocks.push(deserialize(req.body));
+    addMockFunctionsToSchema({ schema, mocks: currentMocks });
+    res.sendStatus(200);
+  });
+  app.post('/reset', (req, res) => {
+    currentMocks.splice(1, currentMocks.length);
+    removeMockFunctionsFromSchema({ schema });
+    addMockFunctionsToSchema({ schema, mocks: {} });
+    res.sendStatus(200);
+  });
 
-server.applyMiddleware({ app });
+  server.applyMiddleware({ app });
 
-app.listen({ port }, () => {
-  console.log(`GraphQL Mock Server running ${port}`);
-});
+  app.listen({ port }, () => {
+    console.log(`🐬  GraphQL Mock Server running at http://localhost:${port}`);
+  });
+}
